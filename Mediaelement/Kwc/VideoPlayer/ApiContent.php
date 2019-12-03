9f6248719831b5b08cbb049a7fd170af23bc66c1
Benjamin Hohenwarter <bh@vivid-planet.com>
2019-12-03 12:18:38 +0100

    ContentApi: enable video component

---
+++ Mediaelement/Kwc/VideoPlayer/ApiContent.php
@@ -0,0 +1,22 @@
+<?php
+class Mediaelement_Kwc_VideoPlayer_ApiContent implements Kwf_Component_ApiContent_Interface
+{
+    public function getContent(Kwf_Component_Data $data)
+    {
+        $ret = array();
+        $row = $data->getComponent()->getRow();
+        if ($row->source_type === 'files') {
+            $ret['videoUrl'] = $data->getComponent()->getVideoUrl();
+        } else if ($row->source_type === 'links') {
+            $ret['videoUrl'] = $row->mp4_url;
+        }
+        if ($row->size == 'userDefined') {
+            $ret['videoWidth'] = $row->video_width;
+            $ret['videoHeight'] = $row->video_height;
+        }
+        $ret['autoPlay'] = !!$row->auto_play;
+        $ret['loop'] = !!$row->loop;
+        $ret['format'] = $row->format;
+        return $ret;
+    }
+}
